352336cda2c7560b2e8b9a0829b3ad744cc1ff3b
Brati Pradhan <pradhanbrati@gmail.com>
2020-04-14 15:55:40 +0530

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+ function About() {
+    return (
+        <React.Fragment>
+            <h1>
+                About
+            </h1>
+            <p>
+                In this Todo List, you can add a Todo, delete an existing Todo and mark the Todo. 
+            </p>
+        </React.Fragment>
+    )
+}
+
+
+export default About;
